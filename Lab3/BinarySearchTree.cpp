0199588fe7eef47c1eea62f06f1bb06012f43411
Jotham Chow <Jotham Chow>
2016-03-15 14:37:26 -0400

    more functions and classes

--- Lab3/BinarySearchTree.cpp
+++ Lab3/BinarySearchTree.cpp
@@ -9,31 +9,52 @@
 // Implement the functions here.
 
 BinarySearchTree::BinarySearchTree()
+{
+	
+}
 
-~BinarySearch Tree();
+BinarySearchTree::~BinarySearch Tree();
+{
+}
 
 bool BinarySearchTree::insert(Datatype val)
+{
+}
 
 bool BinarySearchTree::remove(DataType val);
+{
+}
 bool BinarySearchTree::exists(DataType val) const;
+{
+}
 
 DataType BinarySearchTree::BinarySearchTree::min() const;
+{
+}
 
 DataType BinarySearchTree::BinarySearchTree::max() const;
+{
+}
 
 unsigned int BinarySearchTree::sBinarySearchTree::ize() const;
+{
+}
 
 unsigned int BinarySearchTree::depth() const;
+{
+}
 
 void BinarySearchTree::print() const;
+{
+}
 
-private:
+
 	
-	int BinarySearchTree::getNodeDepth(Node* n) const;
+int BinarySearchTree::getNodeDepth(Node* n) const;
 	
 	Node* root_;
 	
 	unsigned int size_;
 	
-	BinarySearchTree(const BinarySearchTree& other) {}
-   BinarySearchTree& operator=(const BinarySearchTree& other) {}
+BinarySearchTree(const BinarySearchTree& other) {}
+BinarySearchTree& operator=(const BinarySearchTree& other) {}
