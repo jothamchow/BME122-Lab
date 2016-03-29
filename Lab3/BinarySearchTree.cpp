b5a30e101cbc23bca3273bedd5355e2c65f8ba01
1rheefra <1rheefra@hdsb.ca>
2016-03-29 15:04:35 -0400

    Francis BST

--- Lab3/BinarySearchTree.cpp
+++ Lab3/BinarySearchTree.cpp
@@ -6,6 +6,7 @@ typedef BinarySearchTree::DataType DataType;
 
 BinarySearchTree::BinarySearchTree()
 {
+	root_ = NULL;
 	size_ = 0;
 }
 
@@ -24,22 +25,32 @@ bool BinarySearchTree::remove(DataType val)
 }
 bool BinarySearchTree::exists(DataType val) const
 {
-
+	
 }
 
 DataType BinarySearchTree::BinarySearchTree::min() const
 {
-	
+	Node* current = root_;
+	while(current -> left != NULL)
+	{
+		current = current -> left;
+	}
+	return (current -> val);
 }
 
 DataType BinarySearchTree::BinarySearchTree::max() const
 {
-	
+	Node* current = root_;
+	while(current -> right != NULL)
+	{
+		current = current -> right;
+	}
+	return (current -> val);
 }
 
 unsigned int BinarySearchTree::BinarySearchTree::size() const
 {
-	
+	return size_;
 }
 
 unsigned int BinarySearchTree::depth() const
@@ -49,6 +60,8 @@ unsigned int BinarySearchTree::depth() const
 
 void BinarySearchTree::print() const
 {
+	cout << "[ ";
+	
 }
 
 int BinarySearchTree::getNodeDepth(Node* n) const
