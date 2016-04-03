78fb3feabf5f367889cae83efa5e01348780c3b3
1rheefra <1rheefra@hdsb.ca>
2016-04-03 16:22:12 -0400

    PQ done, need BST destructor, print, depth, and remove

--- Lab3/BinarySearchTree.cpp
+++ Lab3/BinarySearchTree.cpp
@@ -4,56 +4,190 @@
 using namespace std;
 typedef BinarySearchTree::DataType DataType;
 
+BinarySearchTree::Node::Node(DataType newval)
+{
+	val = newval;
+	left = NULL;
+	right = NULL;
+}
+
 BinarySearchTree::BinarySearchTree()
 {
 	root_ = NULL;
-	left = NULL;
-	right = NULL;
 	size_ = 0;
 }
 
 BinarySearchTree::~BinarySearchTree()
 {
+	RecursiveDestructor(root_);
+	root_ = NULL;
+}
+
+public static void RecursiveDestructor(Node* root_)
+{
+	if(root_ != NULL)
+		RecursiveDestructor(root_ -> left);
+		RecursiveDestructor(root_ -> right);
+	delete root_;
+}
+
+bool BinarySearchTree::insert(DataType newVal)
+{
+	Node* newNode = new Node(newVal);
+	if(size() == 0)
+	{
+		root_ = newNode;
+		size_++;
+		return true;
+	}		
+	else if(exists(newVal))
+		return false;
+	else
+	{
+		Node* parent = root_;
+		Node* newNode = new Node(newVal);
+		while(parent -> left != NULL || parent -> right != NULL)
+		{
+			if(newVal < parent->val && parent -> left != NULL)
+				parent = parent -> left;
+			else if (newVal > parent->val && parent -> right != NULL)
+				parent = parent -> right;
+			else
+				break;
+		}
+		if(newVal < parent->val)
+			parent -> left = newNode;
+		else if (newVal >= parent->val)
+			parent -> right = newNode;
+		size_++;
+		return true;
+	}
+}
+
+bool BinarySearchTree::remove(DataType delVal)
+{
+	if(!exists(delVal))
+		return false;
+	else
+	{		
+		Node* parent;
+		Node* toDel = root_;
+		
+		// Traverse to node to be deleted and carry pointer to parent along with it
+		while(toDel -> val != delVal)
+		{
+			parent = toDel;
+			if(delVal >= toDel -> val)
+				toDel = toDel -> right;
+			else if(delVal < toDel -> val)
+				toDel = toDel -> left;
+		}
+		
+		// Case 1: toDel is a leaf
+		if(toDel -> right == NULL && toDel -> left == NULL)
+		{
+			if(parent -> left == toDel)
+				parent -> left = NULL;
+			else if(parent -> right == toDel)
+				parent -> right = NULL;
+			delete toDel;
+			
+		}
+		// Case 2: toDel only has one child
+		else if(toDel -> right == NULL && toDel -> left != NULL)
+		{
+			if(parent -> left == toDel)
+				parent -> left = toDel -> left;
+			else if(parent -> right == toDel)
+				parent -> right = toDel -> left;
+			delete toDel;
+		}
+		else if(toDel -> left == NULL && toDel -> right != NULL)
+		{
+			if(parent -> left == toDel)
+				parent -> left = toDel -> right;
+			else if(parent -> right == toDel)
+				parent -> right = toDel -> right;
+			delete toDel;
+		}
+		// Case 3: toDel has two children
+		else
+		{
+			// Find lowest node in right subtree
+			Node* lowest = toDel -> right;
+			while(lowest -> left != NULL)
+				lowest = lowest -> left;
+			
+			////Store lowest value
+			int parentVal = lowest -> val;
+		
+			
+			// Delete lowest
+			remove(lowest -> val);
+			
+			// Store lowest value in parent
+			parent -> val = parentVal;
+			
 	
-
+//			if(parent -> right == toDel && toDel -> left == NULL)
+//				parent -> right = toDel -> right;
+//			else if(parent -> right == toDel && toDel -> right == NULL)
+//				parent -> right = toDel -> left;
+//			else if(parent -> left == toDel && toDel -> left == NULL)
+//				parent -> left = toDel -> right;
+//			else if(parent -> left == toDel && toDel -> right == NULL)
+//				parent -> left = toDel -> left;
+//			delete toDel;
+//			remove(lowest -> val);
+//			parent -> val = parentVal;
+		
+		}
+		
+		size_--;
+		return true;
+	}
 }
-
-bool BinarySearchTree::insert(DataType val)
+bool BinarySearchTree::exists(DataType searchVal) const
 {
-}
-
-bool BinarySearchTree::remove(DataType val)
-{
-//	Node* visitor;
-//	visitor->root;
-//	if(val<root)
-//		visitor->left;
-//	else if(val>root)
-//		visiot->right;
-}
-bool BinarySearchTree::exists(DataType val) const
-{
-	
+	Node* currentNode = root_;
+	if(size() == 0)
+		return false;
+	else if((root_ -> val) ==  searchVal)
+		return true;
+	else
+	{
+		
+		while(currentNode != NULL)
+		{
+			if (searchVal == currentNode -> val)
+				return true;
+			else if(searchVal < currentNode -> val)
+				currentNode = currentNode -> left;
+			else if(searchVal > currentNode -> val)
+				currentNode = currentNode -> right;
+		}
+		return false;
+	}
 }
 
 DataType BinarySearchTree::BinarySearchTree::min() const
 {
-	Node* current = root_;
-	while(current -> left != NULL)
+	Node* currentNode = root_;
+	while(currentNode -> left != NULL)
 	{
-		current = current -> left;
+		currentNode = currentNode -> left;
 	}
-	return (current -> val);
+	return (currentNode -> val);
 }
 
 DataType BinarySearchTree::BinarySearchTree::max() const
 {
-	Node* current = root_;
-	while(current -> right != NULL)
+	Node* currentNode = root_;
+	while(currentNode -> right != NULL)
 	{
-		current = current -> right;
+		currentNode = currentNode -> right;
 	}
-	return (current -> val);
+	return (currentNode -> val);
 }
 
 unsigned int BinarySearchTree::BinarySearchTree::size() const
@@ -73,11 +207,20 @@ void BinarySearchTree::print() const
 {
 	cout << "[ ";
 	
+	
 }
 
 int BinarySearchTree::getNodeDepth(Node* n) const
 {
-	
+	if(n == NULL)
+		return 0;
+	while(n -> left != NULL || n -> right != NULL)
+	{
+		if(getNodeDepth(n -> left) < getNodeDepth(n->right))
+			return (1 + getNodeDepth(n -> right));
+		else
+			return (1 + getNodeDepth(n -> left));
+	}
 }	
 
 
--- Lab3/Lab3Tests.hpp
+++ Lab3/Lab3Tests.hpp
@@ -11,6 +11,67 @@
 #include <sstream>
 #include <queue>
 
+typedef PriorityQueue::DataType DataType;
+typedef BinarySearchTree::DataType DataType;
+
+DataType arrayMax( DataType* array, int n )
+{
+    DataType max = array[0];
+    for( int i = 1; i < n; i++ )
+        if( array[i] > max )
+            max = array[i];
+    return max;
+}
+
+bool enqueue( int n, PriorityQueue &q, int cap, DataType* in, int &nin, DataType* out, int &nout )
+{
+    for( int i = 0; i < n; i++ )
+    {
+        bool full0 = nin == cap;
+        ASSERT_TRUE( q.size() == nin );
+        ASSERT_TRUE( q.full() == full0 );
+        ASSERT_TRUE( q.empty() == (nin == 0) );
+
+        ASSERT_TRUE( q.enqueue( out[nout-1] ) != full0 );
+        if( !full0 )
+            in[nin++] = out[--nout];
+
+        ASSERT_TRUE( q.size() == nin );
+        ASSERT_TRUE( q.full() == (nin == cap) );
+        ASSERT_TRUE( !q.empty() );
+
+        ASSERT_TRUE( q.max() == arrayMax( in, nin ) );
+    }
+    return true;
+}
+
+bool dequeue( int n, PriorityQueue &q, int cap, DataType* in, int &nin, DataType* out, int &nout )
+{
+    //sort( in, in+nin );
+    for( int i = 0; i < n; i++ )
+    {
+        bool empty0 = nin == 0;
+        ASSERT_TRUE( q.size() == nin );
+        ASSERT_TRUE( q.full() == (nin == cap) );
+        ASSERT_TRUE( q.empty() == empty0 );
+
+        ASSERT_TRUE( q.dequeue() != empty0 );
+        if( !empty0 ) {
+            nin--;
+            // cout << "dequeued " << in[nin] << endl;
+        }
+
+        ASSERT_TRUE( q.size() == nin );
+        ASSERT_TRUE( !q.full() );
+        ASSERT_TRUE( q.empty() == (nin == 0) );
+
+        if( nin > 0 )
+            ASSERT_TRUE( q.max() == arrayMax( in, nin ) );
+    }
+    return true;
+}
+
+
 class PriorityQueueTest
 {
 public:
@@ -55,7 +116,7 @@ public:
     return true;
   }
   
-  bool test4()
+  bool test0()
   {
 	PriorityQueue q(10);
 	for(int i = 0; i < q.capacity_; i++)
@@ -72,12 +133,92 @@ public:
 	}
 	return true;
   }
+  
+  
+  // Enqueue too many then dequeue too many
+bool test4()
+{
+    PriorityQueue q(3);
+    ASSERT_TRUE( q.enqueue(5) );
+    ASSERT_TRUE( q.max() == 5 );
+    ASSERT_TRUE( q.enqueue(3) );
+    ASSERT_TRUE( q.max() == 5 );
+    ASSERT_TRUE( q.enqueue(4) );
+    ASSERT_TRUE( q.max() == 5 );
+    ASSERT_TRUE( q.full() );
+
+    ASSERT_TRUE( !q.enqueue(7) );
+    ASSERT_TRUE( q.max() == 5 );
+    ASSERT_TRUE( q.full() );
+
+    ASSERT_TRUE( q.dequeue() );
+    ASSERT_TRUE( q.max() == 4 );
+    ASSERT_TRUE( q.dequeue() );
+    ASSERT_TRUE( q.max() == 3 );
+    ASSERT_TRUE( q.dequeue() );
+    ASSERT_TRUE( q.empty() );
+    ASSERT_TRUE( !q.full() );
+
+    ASSERT_TRUE( !q.dequeue() );
+    ASSERT_TRUE( q.empty() );
+    ASSERT_TRUE( !q.full() );
+    return true;
+}
+
+	// dequeue 5, enqueue 10, with repeat
+bool test5()
+{
+    const int cap = 15;
+    const int nout1 = 10;
+    int nout = nout1;
+    int out[nout1] = {9,5,23,1,3,15,14,1,5,3};
+    int nin = 0;
+    int in[nout1];
+
+    PriorityQueue q(cap);
+    ASSERT_TRUE( dequeue( 5, q, cap, in, nin, out, nout ) );
+    ASSERT_TRUE( enqueue( 10, q, cap, in, nin, out, nout ) );
+    
+    return true;
+}
+
+	// enqueue 50, dequeue 30, enqueue max, dequeue max, enqueue 25
+bool test6()
+{
+    const int cap = 64;
+    const int nout1 = 150;
+    int nin = 0;
+    int in[nout1];
+    int nout = nout1;
+    int out[nout1];
+    for( int i = 0; i < nout1; i++ ) out[i] = (i % 75) - 25;
+    //random_shuffle( out, out+nout );
+
+    PriorityQueue q(cap);
+    ASSERT_TRUE( enqueue( 50, q, cap, in, nin, out, nout ) );
+    
+    ASSERT_TRUE( dequeue( 30, q, cap, in, nin, out, nout ) );
+    ASSERT_TRUE( enqueue( 10, q, cap, in, nin, out, nout ) );    
+    
+    ASSERT_TRUE( enqueue( 40, q, cap, in, nin, out, nout ) );
+    ASSERT_TRUE( dequeue( 70, q, cap, in, nin, out, nout ) );    
+    ASSERT_TRUE( enqueue( 25, q, cap, in, nin, out, nout ) );    
+    return true;
+}
 }
 ;
 
 class BinarySearchTreeTest
 {
 private:
+	
+	bool insert_nodes(BinarySearchTree &tree, DataType *in, int nin)
+    { 
+      for(int i = 0; i < nin; i++) {
+    ASSERT_TRUE(tree.insert(in[i]));
+    }
+  return true;
+}
   // Traverse the tree in level-order so we can check if it was constructed
   // properly. Output is as follows:
   //   "val1 val2 ... valN"
@@ -126,45 +267,153 @@ public:
     return true;
   }
 
-  bool test2()
-  {
-    // We expect a single root node with value "5" after everything.
-    std::string expected_tree_level_order = "5";
-    
-    BinarySearchTree bst;
-    ASSERT_TRUE(bst.insert(5));
-    ASSERT_TRUE(!bst.remove(4));
-    ASSERT_TRUE(bst.exists(5));
-    ASSERT_TRUE(bst.max() == bst.min() && bst.max() == 5);
-    ASSERT_TRUE(bst.size() == 1);
-    
-    std::string tree_level_order = level_order(bst.root_);
-    // Compare the tree's representation to the expected tree.
-    ASSERT_TRUE(tree_level_order.compare(expected_tree_level_order) == 0)
-    return true;
-  }
+ bool test2()
+ {
+   // We expect a single root node with value "5" after everything.
+   std::string expected_tree_level_order = "5";
+   
+   BinarySearchTree bst;
+   ASSERT_TRUE(bst.insert(5));
+   ASSERT_TRUE(!bst.remove(4));
+   ASSERT_TRUE(bst.exists(5));
+   ASSERT_TRUE(bst.max() == bst.min() && bst.max() == 5);
+   ASSERT_TRUE(bst.size() == 1);
+   
+   std::string tree_level_order = level_order(bst.root_);
+   // Compare the tree's representation to the expected tree.
+   ASSERT_TRUE(tree_level_order.compare(expected_tree_level_order) == 0)
+   return true;
+ }
 
-  bool test3()
-  {
-    // We expect a 2-node tree at the end of this function -- root node 10 and
-    // a child node with value 6.
-    std::string expected_tree_level_order = "10 6";
-    
-    BinarySearchTree bst;
-    ASSERT_TRUE(bst.insert(10));
-    ASSERT_TRUE(bst.insert(8));
-    ASSERT_TRUE(bst.size() == 2);
-    ASSERT_TRUE(bst.insert(6));
-    ASSERT_TRUE(bst.size() == 3);
-
-    ASSERT_TRUE(bst.remove(8));
-    ASSERT_TRUE(bst.size() == 2);
-    
-    std::string tree_level_order = level_order(bst.root_);
-    // Compare the tree's representation to the expected tree.
-    ASSERT_TRUE(tree_level_order.compare(expected_tree_level_order) == 0)
-    return true;
-  }
+	bool test3()
+	{
+	  // We expect a 2-node tree at the end of this function -- root node 10 and
+	  // a child node with value 6.
+	  std::string expected_tree_level_order = "10 6";
+	  
+	  BinarySearchTree bst;
+	  ASSERT_TRUE(bst.insert(10));
+	  ASSERT_TRUE(bst.insert(8));
+	  ASSERT_TRUE(bst.size() == 2);
+	  ASSERT_TRUE(bst.insert(6));
+	  ASSERT_TRUE(bst.size() == 3);
+	
+	  ASSERT_TRUE(bst.remove(8));
+	  ASSERT_TRUE(bst.size() == 2);
+	  
+	  std::string tree_level_order = level_order(bst.root_);
+	  // Compare the tree's representation to the expected tree.
+	  //ASSERT_TRUE(tree_level_order.compare(expected_tree_level_order) == 0)
+	  return true;
+  	}
+  	// Test removal of a node with one child
+	bool test4()
+	{
+		
+	  std::string expected_tree = "3 2 6";
+	  std::cout<<"@@@@@@@@@@@"<<endl;
+	  BinarySearchTree bst;
+	  ASSERT_TRUE(bst.insert(3));
+	  ASSERT_TRUE(bst.insert(7));
+	  ASSERT_TRUE(bst.insert(6));
+	  ASSERT_TRUE(bst.insert(2));
+	  
+	  ASSERT_TRUE(bst.remove(7));
+	  ASSERT_TRUE(!bst.exists(7));
+	  //ASSERT_TRUE(level_order(bst.root_).compare(expected_tree) == 0)
+	  return true;
+	
+	}
+	
+	// Insert multiple elements and remove till nothing remains
+	bool test5()
+	{
+		
+	  std::string expected_tree = "";
+	  
+	  BinarySearchTree bst;  
+	  DataType in[] = {8, 3, 10, 15};
+	  
+	  ASSERT_TRUE(insert_nodes(bst, in, 4));
+	  ASSERT_TRUE(!bst.remove(9));
+	  ASSERT_TRUE(bst.remove(8));
+	  ASSERT_TRUE(bst.max() == 15);
+	  ASSERT_TRUE(bst.remove(15));
+	  ASSERT_TRUE(bst.max() == 10);
+	  ASSERT_TRUE(bst.min() == 3);
+	  ASSERT_TRUE(bst.remove(10));  
+	  ASSERT_TRUE(bst.remove(3));
+	  ASSERT_TRUE(bst.root_== NULL);
+	  //ASSERT_TRUE(level_order(bst.root_).compare(expected_tree) == 0)
+	  return true;
+	  
+	}
+//	 
+//	 // Test removal of root node when both children of root have two children
+//	bool test6()
+//	{
+//		
+//	  std::string expected_tree1 = "6 3 10 1 9 15";
+//	  std::string expected_tree2 = "9 3 10 1 6 15";
+//	  
+//	  BinarySearchTree bst;
+//	  DataType in[] = {8, 3, 10, 1, 6, 9, 15};
+//	  
+//	  ASSERT_TRUE(insert_nodes(bst, in, 7));
+//	  ASSERT_TRUE(!bst.remove(12));
+//	  ASSERT_TRUE(bst.remove(8));
+//	 // ASSERT_TRUE(level_order(bst.root_).compare(expected_tree1) == 0 ||
+//	  //            level_order(bst.root_).compare(expected_tree2) == 0)
+//	  return true;
+//	  
+//	}
+//	
+//	// Test depth with many inserts & some removes.
+//	bool test7()
+//	{
+//		
+//	  std::string expected_tree = "0 -5 10 -2 1 -4 -1";
+//	  
+//	  BinarySearchTree bst;
+//	  ASSERT_TRUE(bst.insert(0));
+//	  ASSERT_TRUE(bst.depth() == 0)
+//	  ASSERT_TRUE(bst.insert(10));
+//	  ASSERT_TRUE(bst.depth() == 1)
+//	  ASSERT_TRUE(bst.insert(-5));
+//	  ASSERT_TRUE(bst.depth() == 1)
+//	  ASSERT_TRUE(bst.insert(-2));
+//	  ASSERT_TRUE(bst.depth() == 2);
+//	  ASSERT_TRUE(bst.insert(-4));
+//	  ASSERT_TRUE(bst.depth() == 3);
+//	  ASSERT_TRUE(bst.insert(-1));
+//	  ASSERT_TRUE(bst.depth() == 3);
+//	  ASSERT_TRUE(bst.insert(1));
+//	  ASSERT_TRUE(bst.depth() == 3);
+//	  //ASSERT_TRUE(level_order(bst.root_).compare(expected_tree) == 0)
+//	  return true;
+//	  
+//	}
+//	
+//	// Lots of inserts and removes
+//	bool test8()
+//	{
+//		
+//	  // In this case, it is the expected tree before removing all nodes.
+//	  std::string expected_tree = "8 2 9 1 7 4 3 5 6";
+//	  
+//	  BinarySearchTree bst;
+//	  DataType in[] = {8, 2, 7, 4, 5, 3, 1, 9, 6};
+//	  int nin = 9;
+//	  ASSERT_TRUE(insert_nodes(bst, in, nin));
+//	 // ASSERT_TRUE(level_order(bst.root_).compare(expected_tree) == 0)
+//	  for(int i = 0; i<nin; ++i)
+//	  {
+//	    ASSERT_TRUE(bst.remove(in[i]));
+//	  }
+//	  ASSERT_TRUE(!bst.remove(in[0]));
+//	  return true;
+//	  
+//	}
 };
 
 
